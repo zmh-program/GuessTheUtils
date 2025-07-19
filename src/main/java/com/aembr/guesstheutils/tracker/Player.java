95a7ae98ba3cc4fb9e8849a64a825ca595583f56
aembr <aembr@proton.me>
2025-07-19 23:29:16 +0100

    add smed sc for blacksmith

---
+++ src/main/java/com/aembr/guesstheutils/tracker/Player.java
@@ -0,0 +1,6 @@
+package com.aembr.guesstheutils.tracker;
+
+public class Player {
+    public enum Ranks { NON, VIP, VIP_P, MVP, MVP_P, MVP_PP, YT }
+    public enum Titles {}
+}
---
+++ src/main/java/com/aembr/guesstheutils/tracker/Tracker.java
@@ -0,0 +1,4 @@
+package com.aembr.guesstheutils.tracker;
+
+public class Tracker {
+}
